b40e335414f63fe49155526ad50a5ba9d99522e1
Moawiya <111635333+Moawiya-Sajjad@users.noreply.github.com>
2023-10-17 21:29:29 +0800

    All Files Uploaded

---
+++ map.js
@@ -0,0 +1,18 @@
+var map_1 = "map.vg.json";
+var linechart_1 = "linechart.vg.json";
+var stackedbarchart_1 = "stackedbarchart.vg.json";
+var donutchart_1 = "donutchart.vg.json";
+var donutchart2_1 = "donutchart2.vg.json";
+var slopechart_1 = "slopechart.vg.json";
+var bumpchart_1 = "bumpchart.vg.json";
+var histogram_1 = "histogram.vg.json";
+
+
+vegaEmbed("#map", map_1).then(function (result) {}).catch(console.error);
+vegaEmbed("#linechart", linechart_1).then(function (result) {}).catch(console.error);
+vegaEmbed("#stackedbarchart", stackedbarchart_1).then(function (result) {}).catch(console.error);
+vegaEmbed("#donutchart", donutchart_1).then(function (result) {}).catch(console.error);
+vegaEmbed("#donutchart2", donutchart2_1).then(function (result) {}).catch(console.error);
+vegaEmbed("#slopechart", slopechart_1).then(function (result) {}).catch(console.error);
+vegaEmbed("#bumpchart", bumpchart_1).then(function (result) {}).catch(console.error);
+vegaEmbed("#histogram", histogram_1).then(function (result) {}).catch(console.error);
